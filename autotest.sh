5a6a59dc6a1cd8c1ed6e5be0dd5f90e350545e73
seigot <you@example.com>
2020-07-31 02:08:07 +0000

    update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -2,19 +2,21 @@
 
 cd ~/catkin_ws/src/burger_war
 
-echo "time, my_score, enemy_score" > result.log
+echo "iteration, enemy_level, game_time(s), date, my_score, enemy_score, battle_result" > result.log
 
-LOOP_N=30
+LOOP_TIMES=1
 
 function do_game(){
-    GAME_TIME=$1
+    ITERATION=$1
+    ENEMY_LEVEL=$2
+    GAME_TIME=$3
 
     #start
     gnome-terminal -e "bash scripts/sim_with_judge.sh"
     sleep 30
-    gnome-terminal -e "bash scripts/start.sh"
+    gnome-terminal -e "bash scripts/start.sh -l ${ENEMY_LEVEL}"
 
-    #game
+    #wait game finish
     sleep $GAME_TIME
 
     #get result
@@ -22,7 +24,14 @@ function do_game(){
     MY_SCORE=`cat out.log | grep -w my_score | cut -d'=' -f2`
     ENEMY_SCORE=`cat out.log | grep -w enemy_score | cut -d'=' -f2`
     DATE=`date --iso-8601=seconds`
-    echo "$DATE, $MY_SCORE, $ENEMY_SCORE" >> result.log
+    BATTLE_RESULT="LOSE"
+    if [ $MY_SCORE > $ENEMY_SCORE ]; then
+	BATTLE_RESULT="WIN"
+    fi
+
+    #output result
+    echo "$ITERATION, $ENEMY_LEVEL, $GAME_TIME, $DATE, $MY_SCORE, $ENEMY_SCORE, $BATTLE_RESULT" >> result.log
+    tail -1  result.log
     
     #stop
     PROCESS_ID=`ps -e -o pid,cmd | grep start.sh | grep -v grep | awk '{print $1}'`
@@ -37,8 +46,9 @@ function do_game(){
 }
 
 # main loop
-LOOP_TIMES=40
 for ((i=0; i<${LOOP_TIMES}; i++));
 do
-    do_game 225 # 180 * 5/4 
+    do_game ${i} 1 225 # 180 * 5/4 
+    do_game ${i} 2 225 # 180 * 5/4 
+    do_game ${i} 3 225 # 180 * 5/4
 done
